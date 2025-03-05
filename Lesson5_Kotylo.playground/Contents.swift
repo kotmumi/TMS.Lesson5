4d7760a635218dfa06202df3f6a575bdfb438e8f
Kotylo <kirillkotylo@gmail.com>
2025-03-05 23:51:05 +0300

    create enum

--- Lesson5_Kotylo.playground/Contents.swift
+++ Lesson5_Kotylo.playground/Contents.swift
@@ -1,2 +1,13 @@
+import Foundation
 
+// MARK: - 1. Конвертер валют с обработкой ошибок:
 
+//•     Создайте enum Currency с кейсами для USD, EUR, BYN. Каждому кейсу присвойте rawValue типа String с соответствующим кодом валюты.
+//•     Создайте функцию getExchangeRate(from: Currency, to: Currency) -> Double?, которая принимает две валюты и возвращает курс обмена между ними. Если для какой-то пары валют курс не найден, функция должна возвращать nil.
+//•   Создайте функцию convertCurrency(amount: Double, from: Currency, to: Currency) -> Double?, которая принимает сумму, исходную валюту и целевую валюту и возвращает сконвертированную сумму. Используйте функцию getExchangeRate для получения курса обмена. Если курс обмена не найден, функция должна возвращать nil.
+
+enum Currency: String {
+    case USD = "USD"
+    case EUR = "EUR"
+    case BYN = "BYN"
+}
